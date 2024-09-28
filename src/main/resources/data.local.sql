5c04d21fa1df3610bad4916fa33a10fadc1e2d87
ansrl0107 <ansrl0107@gmail.com>
2024-09-28 17:46:41 +0900

    chore: 로컬환경에서의 초기 데이터 설정

---
+++ src/main/resources/data.local.sql
@@ -0,0 +1,4 @@
+insert into cart (id, code, created_at, updated_at) values
+    (RANDOM_UUID(), 'cart-test_1', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+    (RANDOM_UUID(), 'cart-test_2', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+    (RANDOM_UUID(), 'cart-test_3', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
