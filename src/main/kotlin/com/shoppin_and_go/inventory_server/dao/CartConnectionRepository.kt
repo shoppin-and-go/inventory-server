230bea42b5289d12f2ecf2aff0b636b86791d8d7
ansrl0107 <ansrl0107@gmail.com>
2024-09-28 17:45:23 +0900

    feat: repository 추가

---
+++ src/main/kotlin/com/shoppin_and_go/inventory_server/dao/CartConnectionRepository.kt
@@ -0,0 +1,14 @@
+package com.shoppin_and_go.inventory_server.dao
+
+import com.shoppin_and_go.inventory_server.domain.CartConnection
+import com.shoppin_and_go.inventory_server.domain.DeviceId
+import org.springframework.data.repository.Repository
+import java.util.*
+
+interface CartConnectionRepository : Repository<CartConnection, UUID> {
+    fun findByDeviceIdAndDisconnectedAtIsNull(deviceId: DeviceId): List<CartConnection>
+    fun findByDeviceIdOrderByCreatedAtDesc(deviceId: DeviceId): List<CartConnection>
+    fun existsByDeviceIdAndDisconnectedAtIsNull(deviceId: DeviceId): Boolean
+    fun save(cartConnection: CartConnection): CartConnection
+    fun saveAll(cartConnections: Iterable<CartConnection>): List<CartConnection>
+}
---
+++ src/main/kotlin/com/shoppin_and_go/inventory_server/dao/CartRepository.kt
@@ -0,0 +1,12 @@
+package com.shoppin_and_go.inventory_server.dao
+
+import com.shoppin_and_go.inventory_server.domain.Cart
+import com.shoppin_and_go.inventory_server.domain.CartCode
+import org.springframework.data.repository.Repository
+import java.util.UUID
+
+interface CartRepository : Repository<Cart, UUID> {
+    fun findByCode(code: CartCode): Cart?
+    fun save(cart: Cart): Cart
+    fun deleteAllByCode(code: CartCode)
+}
