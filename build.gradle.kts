d0f1dc9c37e850b8c3a7faad7d4d8e8431d4e031
ansrl0107 <ansrl0107@gmail.com>
2024-09-28 17:37:05 +0900

    chore: application 설정 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ repositories {
 }
 
 val kotestVersion = "5.9.1"
+val kotestExtensionSpringVersion = "1.3.0"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
@@ -27,6 +28,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-websocket")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("com.github.f4b6a3:ulid-creator:5.2.3")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
@@ -34,9 +36,10 @@ dependencies {
 	testImplementation("io.kotest:kotest-assertions-core:${kotestVersion}")
 	testImplementation("io.kotest:kotest-property:${kotestVersion}")
 	testImplementation("io.mockk:mockk:1.13.12")
-	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.3.0")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:${kotestExtensionSpringVersion}")
 	testImplementation("com.navercorp.fixturemonkey:fixture-monkey-starter-kotlin:1.0.25")
-
+	implementation("com.h2database:h2:2.3.232")
+	testImplementation("com.ninja-squad:springmockk:3.1.1")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
