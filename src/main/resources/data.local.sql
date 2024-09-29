64ecf3e70aaf0a1ebb0950993978e0f354316bbd
ansrl0107 <ansrl0107@gmail.com>
2024-09-29 18:26:34 +0900

    chore: Add initial cart connections

--- src/main/resources/data.local.sql
+++ src/main/resources/data.local.sql
@@ -2,3 +2,8 @@ insert into cart (id, code, created_at, updated_at) values
     (RANDOM_UUID(), 'cart-test_1', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
     (RANDOM_UUID(), 'cart-test_2', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
     (RANDOM_UUID(), 'cart-test_3', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+
+insert into cart_connection (id, cart_id, device_id, created_at, updated_at) values
+    (RANDOM_UUID(), (select id from cart where code = 'cart-test_1'), 'device-test_1', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+    (RANDOM_UUID(), (select id from cart where code = 'cart-test_2'), 'device-test_2', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+    (RANDOM_UUID(), (select id from cart where code = 'cart-test_3'), 'device-test_3', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
