7e9f11b80065da068903f1b455702f59a8e8009b
ansrl0107 <ansrl0107@gmail.com>
2024-10-02 16:17:54 +0900

    chore: 원하는 deviceId로 socket test를 할 수 있도록 변경

--- src/main/resources/data.local.sql
+++ src/main/resources/data.local.sql
@@ -7,3 +7,8 @@ insert into cart_connection (id, cart_id, device_id, created_at, updated_at) val
     (RANDOM_UUID(), (select id from cart where code = 'cart-test_1'), 'device-test_1', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
     (RANDOM_UUID(), (select id from cart where code = 'cart-test_2'), 'device-test_2', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
     (RANDOM_UUID(), (select id from cart where code = 'cart-test_3'), 'device-test_3', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+
+insert into product (id, code, name, price, created_at, updated_at) values
+    (RANDOM_UUID(), 'product-test_1', 'Apple', 100.00, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+    (RANDOM_UUID(), 'product-test_2', 'Banana', 200.00, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+    (RANDOM_UUID(), 'product-test_3', 'Carrot', 300.00, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
