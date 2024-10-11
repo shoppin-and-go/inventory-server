7ff20573bb3e8d3f1987a3d17314bacb9e7b7f80
ansrl0107 <ansrl0107@gmail.com>
2024-10-11 12:11:02 +0900

    bug: 구현되지 않은 코드 삭제

--- src/main/kotlin/com/shoppin_and_go/inventory_server/api/InventoryController.kt
+++ src/main/kotlin/com/shoppin_and_go/inventory_server/api/InventoryController.kt
@@ -2,20 +2,17 @@ package com.shoppin_and_go.inventory_server.api
 
 import com.shoppin_and_go.inventory_server.application.InventoryCommandService
 import com.shoppin_and_go.inventory_server.application.InventoryQueryService
-import com.shoppin_and_go.inventory_server.application.OrderService
 import com.shoppin_and_go.inventory_server.domain.CartCode
 import com.shoppin_and_go.inventory_server.domain.DeviceId
 import com.shoppin_and_go.inventory_server.dto.ApiResponse
 import com.shoppin_and_go.inventory_server.dto.CartInventoryStatus
 import com.shoppin_and_go.inventory_server.dto.InventoryUpdateRequest
-import com.shoppin_and_go.inventory_server.dto.OrderDetails
 import org.springframework.web.bind.annotation.*
 
 @RestController
 class InventoryController(
     private val inventoryCommandService: InventoryCommandService,
     private val inventoryQueryService: InventoryQueryService,
-    private val orderService: OrderService,
 ) {
     @PatchMapping("/carts/{cartCode}/inventories")
     fun updateCartInventory(
@@ -32,16 +29,6 @@ class InventoryController(
     ): ApiResponse<CartInventoryStatus> {
         inventoryQueryService.listInventory(deviceId, cartCode).let {
             return ApiResponse.success("inventory", it)
-        }
-    }
-
-    @PostMapping("/devices/{deviceId}/carts/{cartCode}/orders")
-    fun createOrder(
-        @PathVariable deviceId: DeviceId,
-        @PathVariable cartCode: CartCode,
-    ): ApiResponse<OrderDetails> {
-        orderService.createOrder(deviceId, cartCode).let {
-            return ApiResponse.success("order", it)
         }
     }
 }
