c3b7d10659a162db8149f261335b3132c3988e62
ansrl0107 <ansrl0107@gmail.com>
2024-10-19 14:36:03 +0900

    test: CartCode, ProductCode의 fixture strategy 설정

--- src/test/kotlin/com/shoppin_and_go/inventory_server/utils/FixtureBuilders.kt
+++ src/test/kotlin/com/shoppin_and_go/inventory_server/utils/FixtureBuilders.kt
@@ -3,13 +3,21 @@ package com.shoppin_and_go.inventory_server.utils
 import com.navercorp.fixturemonkey.FixtureMonkey
 import com.navercorp.fixturemonkey.kotlin.KotlinPlugin
 import com.navercorp.fixturemonkey.kotlin.giveMeBuilder
-import com.shoppin_and_go.inventory_server.domain.CartCode
-import com.shoppin_and_go.inventory_server.domain.DeviceId
-import com.shoppin_and_go.inventory_server.domain.ProductCode
+import com.navercorp.fixturemonkey.kotlin.setExp
+import com.shoppin_and_go.inventory_server.domain.*
+import net.jqwik.api.Arbitraries
 import java.util.*
 
 object FixtureBuilders {
-    val fixtureMonkey: FixtureMonkey = FixtureMonkey.builder().plugin(KotlinPlugin()).build()
+    val fixtureMonkey: FixtureMonkey = FixtureMonkey.builder()
+        .plugin(KotlinPlugin())
+        .register(Cart::class.java) {
+            it.giveMeBuilder<Cart>().setExp(Cart::code, Arbitraries.create(FixtureBuilders::cartCode))
+        }
+        .register(Product::class.java) {
+            it.giveMeBuilder<Product>().setExp(Product::code, Arbitraries.create(FixtureBuilders::productCode))
+        }
+        .build()
 
     inline fun <reified T> builder() = fixtureMonkey.giveMeBuilder<T>()
     inline fun <reified T> sample(): T = builder<T>().sample()
