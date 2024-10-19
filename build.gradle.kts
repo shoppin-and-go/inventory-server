dc154e942e21d11e6b307c44fce1580ef0dedbdd
ansrl0107 <ansrl0107@gmail.com>
2024-10-19 15:01:37 +0900

    test: spring-restdocs 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.epages.restdocs.apispec.gradle.OpenApi3Task
+
 plugins {
 	kotlin("jvm") version "1.9.25"
 	kotlin("plugin.spring") version "1.9.25"
@@ -5,6 +7,7 @@ plugins {
 	id("org.springframework.boot") version "3.3.4"
 	id("io.spring.dependency-management") version "1.1.6"
 	id("com.google.cloud.tools.jib") version "3.4.3"
+	id("com.epages.restdocs-api-spec") version "0.18.2"
 }
 
 group = "com.shoppin-and-go"
@@ -41,6 +44,8 @@ dependencies {
 	testImplementation("io.mockk:mockk:1.13.12")
 	testImplementation("io.kotest.extensions:kotest-extensions-spring:${kotestExtensionSpringVersion}")
 	testImplementation("com.navercorp.fixturemonkey:fixture-monkey-starter-kotlin:1.0.25")
+	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+	testImplementation("com.epages:restdocs-api-spec-mockmvc:0.18.2")
 	implementation("com.h2database:h2:2.3.232")
 	testImplementation("com.ninja-squad:springmockk:3.1.1")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -50,10 +55,6 @@ kotlin {
 	compilerOptions {
 		freeCompilerArgs.addAll("-Xjsr305=strict")
 	}
-}
-
-tasks.withType<Test> {
-	useJUnitPlatform()
 }
 
 jib {
@@ -87,4 +88,28 @@ jib {
 			"SERVER_PORT" to "8080",
 		)
 	}
+}
+
+openapi3 {
+	title = "Shoppin&Go Inventory API"
+	format = "yaml"
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.register<Delete>("cleanGeneratedSnippets") {
+	delete("build/generated-snippets")
+}
+
+tasks.register<Copy>("copyOasToSwagger") {
+	delete("src/main/resources/static/swagger-ui/openapi3.yaml")
+	from("build/api-spec/openapi3.yaml")
+	into("src/main/resources/static/swagger-ui/.")
+}
+
+tasks.withType<OpenApi3Task>().configureEach {
+	dependsOn("cleanGeneratedSnippets")
+	finalizedBy("copyOasToSwagger")
 }
