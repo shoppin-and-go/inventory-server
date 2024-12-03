8a623d9a657f4e89e5948009df9e790cf24ef8a0
ansrl0107 <ansrl0107@gmail.com>
2024-12-03 12:13:56 +0900

    fix: cors

---
+++ src/main/kotlin/com/shoppin_and_go/inventory_server/config/WebConfig.kt
@@ -0,0 +1,15 @@
+package com.shoppin_and_go.inventory_server.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowCredentials(true)
+    }
+}
